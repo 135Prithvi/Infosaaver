182c70120e69118921c8c21009054db34b1546ed
135Prithvi <prithvi128717@gmail.com>
2022-04-23 15:46:40 +0530

    adding files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: 'mongodb://localhost:27017',
+  },
 }
+// mongodb+srv://PrithvirajIndiaOpenJs:qwertyuiopasdfghjkl@cluster0.rlavh.mongodb.net/test
